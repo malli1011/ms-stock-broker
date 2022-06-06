3512a6a769053e6b09ebcc34f1de586c760a8196
malli1011 <malli1011>
2022-06-06 11:20:12 +0000

    Initial commit

---
+++ src/main/java/com/ms/Application.java
@@ -0,0 +1,9 @@
+package com.ms;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
